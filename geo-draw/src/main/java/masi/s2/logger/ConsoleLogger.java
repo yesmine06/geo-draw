d79f65f0d0d621029cd78e90046c741b5486934c
yesmine06 <zribiyesmine@gmail.com>
2025-06-09 20:52:09 +0100

    La structure du projet est maintenant plus claire et mieux organisée. Vous pouvez continuer à travailler sur le projet avec cette nouvelle organisation.

--- geo-draw/src/main/java/masi/s2/logger/ConsoleLogger.java
+++
@@ -1,8 +0,0 @@
-package masi.s2.logger;
-
-public class ConsoleLogger implements Logger {
-    @Override
-    public void log(String message) {
-        System.out.println(message);
-    }
-} 
--- geo-draw/src/main/java/masi/s2/logger/Logger.java
+++
@@ -1,5 +0,0 @@
-package masi.s2.logger;
-
-public interface Logger {
-    void log(String message);
-} 
